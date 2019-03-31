587e0a4e003a99756f87302be2605d03361c1e6a
Kathleen Xue <kxue@usc.edu>
2019-03-30 23:41:59 -0700

    format

--- djmaps/maps/templates/static/dbscan.py
+++ djmaps/maps/templates/static/dbscan.py
@@ -16,9 +16,9 @@ def convertLatLonToMiles(lat1, lon1, lat2, lon2):
 	d = R * c
 	return d * 0.621371
 
-with open('dataCrime1.json') as json_file:
+with open('./dataCrime1.json', 'r+') as json_file:
 	data = json.load(json_file)
-	distBetweenPoints = []
+	distBetweenPoints = [1]
 
 	miles_per_radian = 3958.7613
 
@@ -26,12 +26,11 @@ with open('dataCrime1.json') as json_file:
 	for f in data['features']:
 		latLong = (f['properties']['latitude'], f['properties']['longitude'])
 		x.append(latLong)
+		f["properties"]["0cluster"] = "-1"
 		#print('Latitude: ' + str(f['properties']['latitude']))
 		#print('Longitude: ' + str(f['properties']['longitude']))
-	
 
 	epsilon = 1
-
 
 	for i in distBetweenPoints: 
 		epsilon = i / miles_per_radian
@@ -40,9 +39,19 @@ with open('dataCrime1.json') as json_file:
 		numberClusters = len(set(labels)) - (1 if -1 in labels else 0)
 		fileName = str(i) + "miles.txt"
 		curFile = open(fileName, "w")
+		k = 0
+		for g in data['features']:
+			clusterLabel = str(i) + "cluster"
+			#print("clusterLabel: " + clusterLabel)
+			g["properties"][clusterLabel] = labels[k]
+			k += 1
+		
 		for j in labels:
 			curFile.write('%s\n' % j)
 		print(numberClusters)
+
+with open('./dataCrime1.json', "w") as json_data:
+	json.dump(data, json_data, indent=4, sort_keys=True, default=str)
 
 #array([ 0,  0,  0,  1,  1, -1])
 #DBSCAN(algorithm='auto', eps=3, leaf_size=30, metric='euclidean',
