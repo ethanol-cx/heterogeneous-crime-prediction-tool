1d1d1eb2e3eafcfeedba7d0afb1cab72da573013
Chuanxiu Xiong <cxiong@wayfair.com>
2019-06-27 23:42:19 -0400

    virtualenv set up

---
+++ crime_pred_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cx324t/GitHub/heterogeneous-crime-prediction-tool/crime_pred_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
